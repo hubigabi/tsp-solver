dc1deca0990e9b030c06f120a1132a062c052aa5
hubigabi <hubigabi19@gmail.com>
2022-04-22 16:01:03 +0200

    Added drawing route

--- tsp-solver-frontend/src/app/tsp-algorithms/tsp-algorithms.component.ts
+++ tsp-solver-frontend/src/app/tsp-algorithms/tsp-algorithms.component.ts
@@ -76,40 +76,9 @@ export class TspAlgorithmsComponent implements OnInit {
   }
 
   generateCities() {
+    this.routeAlgorithmRows = [];
     this.cities = City.generateRandomCities(this.citiesNumber.value, this.cityMaxX, this.cityMaxY);
     this.drawCities(this.cities)
-  }
-
-  drawCities(cities: City[]) {
-    const widthRate = paper.view.size.width / this.cityMaxX;
-    const heightRate = paper.view.size.height / this.cityMaxY;
-
-    paper.project.clear();
-    for (let city of cities) {
-      let cityCircle = new paper.Path.Circle({
-        center: new paper.Point(city.x * widthRate, city.y * heightRate),
-        radius: 3,
-        fillColor: 'black',
-      });
-      const group = new paper.Group();
-      group.addChild(cityCircle);
-
-      cityCircle.onMouseEnter = function (event: MouseEvent) {
-        const pointText = new paper.PointText(new paper.Point(this.position.x, this.position.y - 12));
-        pointText.justification = 'center';
-        pointText.fontSize = 16;
-        pointText.fillColor = new paper.Color('white');
-        pointText.strokeColor = new paper.Color('gray');
-        pointText.content = 'City: ' + city.id;
-        pointText.name = 'tooltip';
-        this.parent.addChild(pointText);
-      }
-
-      cityCircle.onMouseLeave = function (event: MouseEvent) {
-        let index = this.parent.children.findIndex(value => value.name === 'tooltip');
-        this.parent.children[index].remove();
-      }
-    }
   }
 
   runAlgorithm() {
@@ -152,6 +121,67 @@ export class TspAlgorithmsComponent implements OnInit {
       }
     })
 
+  }
+
+  drawCity(city: City, widthRate: number, heightRate: number) {
+    let cityCircle = new paper.Path.Circle({
+      center: new paper.Point(city.x * widthRate, city.y * heightRate),
+      radius: 3,
+      fillColor: 'black',
+    });
+    const group = new paper.Group();
+    group.addChild(cityCircle);
+
+    cityCircle.onMouseEnter = function (event: MouseEvent) {
+      const pointText = new paper.PointText(new paper.Point(this.position.x, this.position.y - 12));
+      pointText.justification = 'center';
+      pointText.fontSize = 16;
+      pointText.fillColor = new paper.Color('white');
+      pointText.strokeColor = new paper.Color('gray');
+      pointText.content = 'City: ' + city.id;
+      pointText.name = 'tooltip';
+      this.parent.addChild(pointText);
+    }
+
+    cityCircle.onMouseLeave = function (event: MouseEvent) {
+      let index = this.parent.children.findIndex(value => value.name === 'tooltip');
+      this.parent.children[index].remove();
+    }
+  }
+
+  drawCities(cities: City[]) {
+    const widthRate = paper.view.size.width / this.cityMaxX;
+    const heightRate = paper.view.size.height / this.cityMaxY;
+
+    paper.project.clear();
+    for (let city of cities) {
+      this.drawCity(city, widthRate, heightRate);
+    }
+    this.highlightStartingCity(widthRate, heightRate);
+  }
+
+  drawCitiesOrder(citiesOrder: number[]) {
+    const widthRate = paper.view.size.width / this.cityMaxX;
+    const heightRate = paper.view.size.height / this.cityMaxY;
+    paper.project.clear();
+
+    for (let i = 0; i < citiesOrder.length - 1; i++) {
+      const city = this.cities[citiesOrder[i]];
+      this.drawCity(city, widthRate, heightRate);
+
+      const from = new paper.Point(city.x * widthRate, city.y * heightRate);
+      const to = new paper.Point(this.cities[citiesOrder[i + 1]].x * widthRate, this.cities[citiesOrder[i + 1]].y * heightRate);
+      const path = new paper.Path.Line(from, to);
+      path.strokeColor = new paper.Color('blue');
+    }
+
+    this.highlightStartingCity(widthRate, heightRate)
+  }
+
+  private highlightStartingCity(widthRate: number, heightRate: number) {
+    const startingCity = this.cities[0];
+    const path = new paper.Path.Circle(new paper.Point(startingCity.x * widthRate, startingCity.y * heightRate), 10);
+    path.strokeColor = new paper.Color('red');
   }
 
 }
