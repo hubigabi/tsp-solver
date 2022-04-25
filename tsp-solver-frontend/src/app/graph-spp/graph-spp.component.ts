8bd8945f8eb914c88e3060a071490aefd852a4fb
hubigabi <hubigabi19@gmail.com>
2022-04-25 22:53:03 +0200

    Init cytoscape

--- tsp-solver-frontend/src/app/graph-spp/graph-spp.component.ts
+++ tsp-solver-frontend/src/app/graph-spp/graph-spp.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as cytoscape from 'cytoscape';
 
 @Component({
   selector: 'app-graph-spp',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphSppComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    const cy = cytoscape({
+      container: document.getElementById('cy'),
+
+      elements: [
+        {
+          data: {id: 'a'}
+        },
+        {
+          data: {id: 'b'}
+        },
+        {
+          data: {id: 'ab', source: 'a', target: 'b'}
+        }
+      ],
+
+      style: [
+        {
+          selector: 'node',
+          style: {
+            'background-color': '#666',
+            'label': 'data(id)'
+          }
+        },
+
+        {
+          selector: 'edge',
+          style: {
+            'width': 3,
+            'line-color': '#ccc',
+            'target-arrow-color': '#ccc',
+            'target-arrow-shape': 'triangle',
+            'curve-style': 'bezier'
+          }
+        }
+      ],
+
+      layout: {
+        name: 'grid',
+        rows: 1
+      }
+
+    });
   }
 
 }
