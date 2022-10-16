c7aeb37553adff790571e245db4aa4662fe74497
hubigabi <hubigabi19@gmail.com>
2022-10-16 06:52:50 +0200

    Added title

--- tsp-solver-frontend/src/app/app.component.ts
+++ tsp-solver-frontend/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tsp-solver-frontend';
+
+  constructor(private titleService: Title,
+              private translateService: TranslateService) {
+  }
+
+  ngOnInit(): void {
+    this.setTitle();
+
+    this.translateService.onLangChange.subscribe(() => {
+      this.setTitle();
+    });
+  }
+
+  private setTitle() {
+    this.translateService.get('PageTitle').subscribe((title: string) => {
+      this.titleService.setTitle(title);
+    });
+  }
+
 }
